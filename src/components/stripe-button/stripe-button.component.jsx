1a9cba814ed56546ce42b38037e3748668d5bc68
mohit shrivastava <mohitmarkmywords@gmail.com>
2024-04-07 00:03:40 +0530

    first commit

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = 'pk_test_51KeDZmSAcjgtcQvDJsWMdOPOegFdeaI4qPYy8KpS2WV4728ocSTiUtJbtnQ4fPvHHXtemjXAw3T0ZP9wnYQFh2hC00wGUnqRdP';
+
+  const onToken = token => {
+    axios({
+      url: 'payment',
+      method: 'post',
+      data: {
+        amount: priceForStripe,
+        token: token
+      }
+    })
+      .then(response => {
+        alert('succesful payment');
+      })
+      .catch(error => {
+        console.log('Payment Error: ', error);
+        alert(
+          'There was an issue with your payment! Please make sure you use the provided credit card.'
+        );
+      });
+  };
+
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name='Shop-Mart Pvt Ltd.'
+      billingAddress
+      shippingAddress
+      image='https://svgshare.com/i/fGy.svg'
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
